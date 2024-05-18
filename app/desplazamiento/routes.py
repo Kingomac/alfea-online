55d144daa7b199b4f4e79ca2cd2cdaac36badcc7
Kingomac <kingomacyt@gmail.com>
2024-05-18 11:43:53 +0000

    Desplazamientos funcionales

--- app/desplazamiento/routes.py
+++ app/desplazamiento/routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+from flask_login import current_user
 from game_data_loader import salas_grupos_csv
+from .service import desplazar_usuario
 
 bp_desplazamiento = Blueprint('desplazamiento', __name__, url_prefix='/desplazamiento',
                               template_folder='templates', static_folder='static')
@@ -9,3 +11,10 @@ bp_desplazamiento = Blueprint('desplazamiento', __name__, url_prefix='/desplazam
 def index():
     salas = salas_grupos_csv.get_salas_by_grupo()
     return render_template('desplazamiento.html', salas_by_grupo=salas.items())
+
+
+@bp_desplazamiento.route('/desplazar', methods=['POST'])
+def desplazar():
+    id_sala_destino = request.form.get('id_sala_destino')
+    sala_destino = desplazar_usuario(current_user, id_sala_destino)
+    return redirect(url_for('salas.lugar', alias=sala_destino['alias']))
---
+++ app/desplazamiento/service.py
@@ -0,0 +1,13 @@
+from app.usuarios.model import Usuario
+from db import redis_db
+from game_data_loader import salas_csv
+
+
+def desplazar_usuario(usuario: Usuario, id_sala_destino: str) -> None:
+    sala_actual = salas_csv.get_by_id(usuario.sala_actual)
+    sala_destino = salas_csv.get_by_id(id_sala_destino)
+    if sala_actual['grupo'] != sala_destino['grupo']:
+        redis_db.hincrby(f"usuario:{usuario.nombre}",
+                         "monedas", -3)
+    redis_db.hset(f"usuario:{usuario.nombre}", "sala_actual", id_sala_destino)
+    return sala_destino
