28e347054c3d3bf77d277ce50fc329f3780a0819
Kingomac <kingomacyt@gmail.com>
2024-05-20 14:02:40 +0000

    Mejoras de estilos en general

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,9 @@ module.exports = {
       },
       gridTemplateColumns: {
         'middle-3': '2fr 3fr 2fr',
-        'middle-2/5': '2fr 5fr 2fr',
-        'middle-1/5': '1fr 5fr 1fr'
+        'middle-sm': '4fr 5fr 2fr',
+        'middle-md': '4fr 8fr 4fr',
+        'middle-lg': '2fr 10fr 2fr',
       },
       gridTemplateRows: {
         '1fr-auto': '1fr auto'
@@ -17,7 +18,7 @@ module.exports = {
       colors: {
         'wpink': {
           500: '#d42e5d',
-          100: 'ff7aac'
+          100: '#ff7aac'
         },
         'wblue': {
           500: '#6cb6fd',
@@ -28,6 +29,9 @@ module.exports = {
       backgroundImage: {
         'gradient-winx1': 'linear-gradient(135deg, rgba(78,86,170,1) 0%, rgba(197,126,254,1) 16%, rgba(222,128,255,1) 33%, rgba(229,214,254,1) 50%, rgba(207,255,255,1) 66%, rgba(160,205,255,1) 83%, rgba(66,144,200,1) 100%)',
       },
+      dropShadow: {
+        'title': '0 0 .6rem white'
+      }
     },
   },
   plugins: [],
