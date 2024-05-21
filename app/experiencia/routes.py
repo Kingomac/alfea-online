ac666a78e6bffbcdbf0a78aff756de4b361ed4e6
Kingomac <kingomacyt@gmail.com>
2024-05-21 20:56:43 +0000

    Estilos mejorar estadísticas

--- app/experiencia/routes.py
+++ app/experiencia/routes.py
@@ -6,7 +6,7 @@ bp_experiencia = Blueprint(
     'experiencia', __name__, url_prefix='/experiencia', template_folder='templates')
 
 
-@bp_experiencia.route('/', methods=['GET'])
-def gastar_experiencia():
+@bp_experiencia.route('/mejorar-estadisticas', methods=['GET'])
+def mejorar_estadisticas():
     combat_stats = current_user.get_combate_stats()
-    return render_template('gastar_experiencia.html', combat_stats=combat_stats)
+    return render_template('mejorar_estadisticas.html', combat_stats=combat_stats)
